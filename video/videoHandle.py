3a204e0efd4e5d2c72ee8094622380a33f79933d
bulldevil <luyou7989@gmail.com>
2024-05-03 23:12:15 +0800

    Reopen ffmpeg run

--- video/videoHandle.py
+++ video/videoHandle.py
@@ -46,10 +46,10 @@ class videoHandler:
             # 使用ffmpeg切割视频, 对于最后一个片段，不需要指定结束时间
             if i < len(timestamps) - 1:
                 self.logger.log(LogLevel.DEBUG, f"正在处理第{i+1}个片段，时间段为{start_time} - {end_time}，输出文件为{output_filename}")
-                # ffmpeg.input(filename, ss=start_time, to=end_time).output(output_filename, acodec='copy', vcodec='copy', loglevel='warning').run()
+                ffmpeg.input(filename, ss=start_time, to=end_time).output(output_filename, acodec='copy', vcodec='copy', loglevel='warning').run()
             else:
                 self.logger.log(LogLevel.DEBUG, f"正在处理最后一个片段，时间段为{start_time}，输出文件为{output_filename}")
-                # ffmpeg.input(filename, ss=start_time).output(output_filename, acodec='copy', vcodec='copy', loglevel='warning').run()
+                ffmpeg.input(filename, ss=start_time).output(output_filename, acodec='copy', vcodec='copy', loglevel='warning').run()
 
     def split_videos(self, input_files, timestamps_list, file_text_list):
         for i, filename in enumerate(input_files):
