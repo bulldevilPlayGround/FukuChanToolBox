1c680a8257ddb6bc618166c966bb950288a1963a
bulldevil <luyou7989@gmail.com>
2025-07-21 23:16:03 +0800

    remove test in code

--- text/videoInfoParserExcel.py
+++ text/videoInfoParserExcel.py
@@ -85,8 +85,8 @@ class videoInfoParserExcel:
                     print(f"{index + 1}. {timestamp}: {text}")
 
 #test
-debugMode = True
-input_file= '12月10日时间轴.xlsx'
-file = videoInfoParserExcel(verbose=debugMode, file=input_file)
-print(f"len of self.videoInfo: {len(file.videoInfo)}")
-file.__debug_print_video_info__()
+# debugMode = True
+# input_file= '12月10日时间轴.xlsx'
+# file = videoInfoParserExcel(verbose=debugMode, file=input_file)
+# print(f"len of self.videoInfo: {len(file.videoInfo)}")
+# file.__debug_print_video_info__()
