066648138bc1ede00a002bf983fc3834f4af3f18
bulldevil <luyou7989@gmail.com>
2025-09-15 20:43:45 +0800

    refine shall not replace dir but only name

--- utils/naming.py
+++ utils/naming.py
@@ -1,4 +1,4 @@
-
+import os
 
 def add_suffix(filename, suffix):
     file_name = '.'.join(filename.split('.')[:-1])
@@ -10,5 +10,14 @@ def change_file_type(filename, new_type):
     return file_name + '.' + new_type
 
 def refine_file_name(filename):
-    # 去除文件名中不能作为windows文件名的字符
-    return filename.replace('/', '_').replace('\\', '_').replace(':', '_').replace('*', '_').replace('?', '_').replace('"', '_').replace('<', '_').replace('>', '_').replace('|', '_')
+    # Split the path into directory and filename
+    directory, base_name = os.path.split(filename)
+    print(f"dir is{directory} and name is {base_name}")
+
+    # Replace invalid Windows filename characters in the base name only
+    invalid_chars = '<>:"*?|\\/'
+    for char in invalid_chars:
+        base_name = base_name.replace(char, '_')
+
+    # Reconstruct the full path
+    return os.path.join(directory, base_name)
